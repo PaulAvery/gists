755a3bc90ad52b2a3364d40e05bc4191ac4b0a5b
Florian Albertz <flo.albertz@gmail.com>
2014-07-12 12:58:07 +0200

    Default to disk

--- config/connections.js
+++ config/connections.js
@@ -1,13 +1,5 @@
 module.exports.connections = {
-	disk: {
+	default: {
 		adapter: 'sails-disk'
-	},
-
-	psql: {
-		adapter	: 'sails-postgresql',
-		host	: 'YOUR_POSTGRES_SERVER_HOSTNAME_OR_IP_ADDRESS',
-		user	: 'YOUR_POSTGRES_USER',
-		password: 'YOUR_POSTGRES_PASSWORD', 
-		database: 'YOUR_POSTGRES_DB'
 	}
 };
